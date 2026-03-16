1e76783b39aa72db071c75e8a9eea3f9cc3f4a1b
Adrián Uceda Espejo <aue0001@alu.medac.es>
2026-03-16 23:54:34 +0100

    Main por fin creado

---
+++ roomiefind/lib/screens/screen.dart
@@ -0,0 +1,2 @@
+export 'role_selection_screen.dart';
+export 'log_In.dart';
--- roomiefind/lib/theme/app_theme.dart
+++ roomiefind/lib/theme/app_theme.dart
@@ -1,2 +1,5 @@
 import 'package:flutter/material.dart';
-
+final ThemeData myAppTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);

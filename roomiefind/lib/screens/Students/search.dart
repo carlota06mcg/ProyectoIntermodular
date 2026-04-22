c7e55f5060fb030221ce88147ea080d73fa7cbec
jvy0001@alu.medac.es <jossFVY@gmail.com>
2026-04-22 18:59:46 +0200

    Base Prototipo
    Base de prototipo

--- roomiefind/lib/screens/Students/search.dart
+++ roomiefind/lib/screens/Students/search.dart
@@ -4,6 +4,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
 
+
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
